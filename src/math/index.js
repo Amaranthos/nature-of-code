7389e911cbd67463df767c357987b1b30a1d18a4
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2020-03-01 15:56:01 +1000

    Add map_range

--- src/math/index.js
+++ src/math/index.js
@@ -7,6 +7,15 @@
  */
 const lerp = (start, end, step) => (1.0 - step) * start + step * end;
 
+/**
+ *
+ * @param {number} x
+ * @param {number} min
+ * @param {number} max
+ */
+const map_range = (x, [a, b], [c, d]) => ((x - a) / (b - a)) * (d - c) + c;
+
 export const math = {
-  lerp
+  lerp,
+  map_range
 };
