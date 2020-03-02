e53f779914b32ff6904c5574eccfd740b3800f00
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2020-03-02 20:25:01 +1000

    Fix clear using wrong axes

--- src/context/clear.js
+++ src/context/clear.js
@@ -2,5 +2,5 @@
  * @param {CanvasRenderingContext2D} ctx
  */
 export const clear = ctx => () => {
-  ctx.clearRect(0, 0, ctx.canvas.height, ctx.canvas.width);
+  ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
 };
