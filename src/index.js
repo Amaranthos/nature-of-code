21a0b16b46a8a9863c93599b4e47726d84246d96
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2021-08-19 18:33:16 +1000

    [1.5] Visualise 2D perlin noise

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
 import * as p5 from "p5";
 
-let start = 0;
-const inc = 0.005;
+const inc = 0.01;
 
 new p5((s) => {
   s.setup = function () {
     s.createCanvas(400, 400);
+    s.pixelDensity(1);
   };
 
   s.draw = function () {
-    s.background(51);
+    s.loadPixels();
 
-    s.stroke(255);
-    s.noFill();
+    let yOff = 0;
+    for (let y = 0; y < s.width; y++) {
+      let xOff = 0;
+      for (let x = 0; x < s.width; x++) {
+        const idx = (x + y * s.width) * 4;
+        const r = s.noise(xOff, yOff) * 255;
+        s.pixels[idx + 0] = r;
+        s.pixels[idx + 1] = r;
+        s.pixels[idx + 2] = r;
+        s.pixels[idx + 3] = 255;
 
-    let xOff = start;
-    s.beginShape();
-    for (let x = 0; x < s.width; x++) {
-      s.stroke(255);
-      const y = s.noise(xOff) * s.height;
-      s.vertex(x, y);
-
-      xOff += inc;
+        xOff += inc;
+      }
+      yOff += inc;
     }
-    s.endShape();
-
-    start += inc;
-
+    s.updatePixels();
     // s.noLoop();
   };
 });
