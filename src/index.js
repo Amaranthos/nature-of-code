4acc309688131b853a289341537e29f85e7e77d5
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2021-08-23 09:43:20 +1000

    [1.2] Vector addition

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import { Walker } from "./walker";
 
 new p5((s) => {
   const walker = new Walker(
-    s.createVector(window.innerWidth * 0.5, window.innerHeight * 0.5)
+    s,
+    window.innerWidth * 0.5,
+    window.innerHeight * 0.5
   );
 
   s.windowResized = function () {
@@ -14,12 +16,11 @@ new p5((s) => {
   s.setup = function () {
     s.createCanvas(window.innerWidth, window.innerHeight);
     s.pixelDensity(1);
-    s.background(51);
   };
 
   s.draw = function () {
-    s.stroke(255);
-    s.point(walker.pos.x, walker.pos.y);
-    walker.update(s.createVector(s.random(-1, 1), s.random(-1, 1)));
+    s.background(51);
+    walker.update();
+    walker.draw();
   };
 });
--- src/walker.js
+++ src/walker.js
@@ -1,7 +1,16 @@
-export const Walker = function (pos) {
-  this.pos = pos;
+export const Walker = function (s, x, y) {
+  this.s = s;
+  this.pos = s.createVector(x, y);
+  this.vel = s.createVector(1, -1);
 
-  this.update = function (next) {
-    this.pos = this.pos.add(next);
+  this.update = function () {
+    this.pos = this.pos.add(this.vel);
+  };
+
+  this.draw = function () {
+    this.s.stroke(255);
+    this.s.strokeWeight(2);
+    this.s.fill(255, 100);
+    this.s.ellipse(this.pos.x, this.pos.y, 32);
   };
 };
