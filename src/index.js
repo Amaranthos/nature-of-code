7dd9988bf5126fb309bd8edf857eb451a0f76253
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2021-08-23 09:55:11 +1000

    [1.3] Random vectors

--- src/index.js
+++ src/index.js
@@ -2,25 +2,23 @@ import * as p5 from "p5";
 import { Walker } from "./walker";
 
 new p5((s) => {
-  const walker = new Walker(
-    s,
-    window.innerWidth * 0.5,
-    window.innerHeight * 0.5
-  );
-
   s.windowResized = function () {
     s.resizeCanvas(window.innerWidth, window.innerHeight);
-    s.background(51);
   };
 
   s.setup = function () {
     s.createCanvas(window.innerWidth, window.innerHeight);
     s.pixelDensity(1);
+    s.background(0);
   };
 
   s.draw = function () {
-    s.background(51);
-    walker.update();
-    walker.draw();
+    s.translate(s.width / 2, s.height / 2);
+
+    let v = p5.Vector.random2D().mult(100);
+
+    s.strokeWeight(4);
+    s.stroke(255);
+    s.line(0, 0, v.x, v.y);
   };
 });
